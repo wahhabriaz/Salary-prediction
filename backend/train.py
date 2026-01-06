1a07984dfb107d8696cba52eae77a9ae6ee59ce5
ahmedriaz12 <93725258+wahhabriaz@users.noreply.github.com>
2026-01-07 04:59:19 +0500

    Update train.py

--- backend/train.py
+++ backend/train.py
@@ -2,7 +2,7 @@ import json
 import joblib
 import numpy as np
 import pandas as pd
-
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
@@ -39,6 +39,8 @@ def train():
 
     slope = float(model.coef_[0])
     intercept = float(model.intercept_)
+
+    os.makedirs("artifacts", exist_ok=True)
 
     # Save model
     joblib.dump(model, MODEL_PATH)
